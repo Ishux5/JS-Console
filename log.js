d470a595a8a4a735a75a97d4f11f7762c4aa042a
Ishux5 <90067359+Ishux5@users.noreply.github.com>
2021-09-29 17:53:37 +0200

    Add files via upload

--- log.js
+++ log.js
@@ -1,18 +1,79 @@
-// Select all VideoTitles
-const titleElements = document.getElementById("contents").querySelectorAll("[id='video-title']");
+// Select all VideoLenghts
+const lengthElements = document
+    .getElementById("contents")
+    .querySelectorAll('[id="text"]');
 
-for(let i = 0; i < titleElements.length; i++){ 
-  titleElements[i].classList.add("video-title")
-};
+for (let i = 0; i < lengthElements.length; i++) {
+    lengthElements[i].classList.add("video-length");
+}
 
-const videoTitle = document.getElementsByClassName("video-title");
-let videoArray = [];
-let numEl = 0;
+const videoLength = document.getElementsByClassName(
+    "style-scope ytd-thumbnail-overlay-time-status-renderer video-length"
+);
 
-for (let i = 0; i < videoTitle.length - 10; i++) {
-    videoArray.push(videoTitle[i].innerText)
-	numEl += 1;
-};
+let totalHours = 0;
+let totalMinutes = 0;
+let totalSeconds = 0;
 
-console.log(videoArray)
-console.log("length: " + numEl)
+for (let i = 0; i < videoLength.length; i++) {
+    let newVideoLength = videoLength[i].innerText;
+    newVideoLength = newVideoLength.replace(/\s/g, "");
+    if (newVideoLength.length < 5) {
+        // if < 0:00
+        newVideoLength = "00:0" + newVideoLength;
+    } else if (newVideoLength.length < 6) {
+        //if < 00:00
+        newVideoLength = "00:" + newVideoLength;
+    } else if (newVideoLength.length < 8) {
+        //if < 0:00:00
+        newVideoLength = "0" + newVideoLength;
+    } else if (newVideoLength.length === 8) {
+    }
+    // newVideoLength Format: hh:mm:ss
+    let tempHour = newVideoLength.charAt(0);
+    tempHour += newVideoLength.charAt(1);
+    let tempMin = newVideoLength.charAt(3);
+    tempMin += newVideoLength.charAt(4);
+    let tempSec = newVideoLength.charAt(6);
+    tempSec += newVideoLength.charAt(7);
+
+    totalHours += parseInt(tempHour);
+    totalMinutes += parseInt(tempMin);
+    totalSeconds += parseInt(tempSec);
+}
+
+// devide Seconds by 60 to add to minutes => only use integer
+const dividendSeconds = Math.floor(totalSeconds / 60);
+const dividendMinutes = Math.floor((totalMinutes + dividendSeconds) / 60);
+
+// add the integer minutes to total Hours
+totalHours += dividendMinutes;
+
+// set rest of the division to the next higher time unit
+const moduloSeconds = totalSeconds % 60;
+const moduloMinutes = (totalMinutes + dividendSeconds) % 60;
+
+// convert time from number to string
+const totalHoursString = totalHours.toString();
+const totalMinutesString = moduloMinutes.toString();
+const totalSecondsString = moduloSeconds.toString();
+
+// add additional character if it is only a single digit
+if (totalHoursString.length < 2) {
+    totalHoursString = "0" + totalHoursString;
+}
+if (totalMinutesString.length < 2) {
+    totalMinutesString = "0" + totalMinutesString;
+}
+if (totalSecondsString.length < 2) {
+    totalSecondsString = "0" + totalSecondsString;
+}
+
+console.log(
+    "The total Time is: " +
+        totalHoursString +
+        ":" +
+        totalMinutesString +
+        ":" +
+        totalSecondsString
+);
